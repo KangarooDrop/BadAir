1c6b1b85c41e3d8736e984d176c9a4c952de3d7d
KangarooDrop <kevinmasse@comcast.net>
2025-10-09 23:06:11 -0400

    Player momentum, Test splatter texture

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,6 +7,11 @@ const SPEED : float = 5.0
 const GRAV : float = 9.8
 const JUMP_FORCE : float = 4.2
 
+const CON_TRAC_GROUND : float = 0.5
+const CON_TRAC_AIR : float = 0.125
+const UNCON_TRAC_GROUND : float = 0.125
+const UNCON_TRAC_AIR : float = 0.017
+
 const SENSITIVITY : float = PI/300.0
 
 var canControl : bool = true
@@ -16,8 +21,9 @@ func _input(event: InputEvent) -> void:
 		return
 	
 	if event is InputEventMouseMotion:
-		head.rotation.y += -event.relative.x * SENSITIVITY
-		head.rotation.x += -event.relative.y * SENSITIVITY
+		var dp : Vector2 = -event.relative
+		head.rotation.y += dp.x * SENSITIVITY
+		head.rotation.x += dp.y * SENSITIVITY
 		head.rotation.x = clampf(head.rotation.x, -PI/2.0, PI/2.0)
 
 func _physics_process(delta: float) -> void:
@@ -29,8 +35,24 @@ func _physics_process(delta: float) -> void:
 		var inputDir : Vector2 = Input.get_vector("left", "right", "forward", "backward").rotated(-head.rotation.y)
 		var moveDir : Vector3 = Vector3(inputDir.x, 0.0, inputDir.y).normalized()
 		
-		velocity.x = moveDir.x * SPEED
-		velocity.z = moveDir.z * SPEED
+		var tracLerpVal : float = 0.0
+		if inputDir.is_zero_approx():
+			if not is_on_floor():
+				tracLerpVal = UNCON_TRAC_AIR
+			else:
+				tracLerpVal = UNCON_TRAC_GROUND
+		else:
+			if not is_on_floor():
+				tracLerpVal = CON_TRAC_AIR
+			else:
+				tracLerpVal = CON_TRAC_GROUND
+		
+		velocity.x = lerp(velocity.x, moveDir.x * SPEED, tracLerpVal)
+		velocity.z = lerp(velocity.z, moveDir.z * SPEED, tracLerpVal)
+		
+		if false:
+			velocity.x = moveDir.x * SPEED
+			velocity.z = moveDir.z * SPEED
 	
 	if not is_on_floor():
 		velocity.y -= GRAV * delta
