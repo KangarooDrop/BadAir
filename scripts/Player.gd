551260f20ee6b1bc536d66300ff9e39924204ef2
KangarooDrop <kevinmasse@comcast.net>
2025-10-09 20:04:12 -0400

    PlayerSpawn and Mushroom Scenes and TrenchBroom entities, AnimatedSprite3DShader for Backlit AnimatedSprite3D

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody3D
+class_name Player
 
 @onready var head : Node3D = $Head
 
--- scripts/World.gd
+++ scripts/World.gd
@@ -1,1 +1,12 @@
-extends Node
+extends Node3D
+
+@onready var player : Player = $Player
+
+func _ready() -> void:
+	if not is_instance_valid(player):
+		return
+	var spawnPoints : Array = get_tree().get_nodes_in_group("PlayerSpawn")
+	if spawnPoints.size() == 0:
+		return
+	var spawnNode : Node3D = spawnPoints[0]
+	player.global_position = spawnNode.global_position
