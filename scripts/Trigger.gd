59d44db2094c1decce7909341bf51f848b65c50c
KangarooDrop <kevinmasse@comcast.net>
2025-10-09 21:55:56 -0400

    BrushDoors and Triggers for Godot and Trenchbroom, Added non-tool variables setting to entity_assembler, Added WorldEnvironment

---
+++ scripts/Trigger.gd
@@ -0,0 +1,33 @@
+extends Area3D
+
+#Used for generic World level triggers
+@export var target : String = ""
+@export var targetname : String = ""
+@export var single_use : bool = false
+
+var targetEntity = null
+var used : bool = false
+
+func _ready() -> void:
+	body_entered.connect(self.onBodyEnter)
+	body_exited.connect(self.onBodyExit)
+	initTarget()
+
+func initTarget() -> void:
+	await get_tree().process_frame
+	var triggerHandlers : Array = get_tree().get_nodes_in_group("TriggerHandler")
+	for th in triggerHandlers:
+		if th.targetname == target:
+			targetEntity = th
+			break
+
+func onBodyEnter(body):
+	if single_use and used:
+		return
+	if is_instance_valid(targetEntity):
+		targetEntity.onTriggerEnter(body, self)
+		used = true
+
+func onBodyExit(body):
+	if is_instance_valid(targetEntity):
+		targetEntity.onTriggerExit(body, self)
